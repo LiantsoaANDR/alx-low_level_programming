e910d588c089058d6fa619568827a64f61d3a71e
LiansoaANDR <liantsoa.cpro@gmail.com>
2023-07-13 08:20:10 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ *main - Entry point
+ *Return 0 Sucess
+ */
+int main(void)
+{
+	char strg[] = "\"Programming is like building a multilingual puzzle,\n";
+	/*Print a string */
+	puts(strg);
+
+	return (0);
+}
