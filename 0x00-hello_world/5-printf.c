61d33e53442734d93403221062099440ec8c5cf1
LiansoaANDR <liantsoa.cpro@gmail.com>
2023-07-13 08:43:09 -0700

    Hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	char strg[] = "with proper grammar, but the outcome is a piece of art,\n";
+	/*Print a string */
+	printf("%s", strg);
+
+	return (0);
+}
