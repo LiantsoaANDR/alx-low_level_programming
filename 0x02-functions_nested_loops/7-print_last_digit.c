6b129ec18265036860b05d9a87fc0fcfd33c6368
LiansoaANDR <liantsoa.cpro@gmail.com>
2023-07-18 18:33:25 -0700

    test

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,11 +8,17 @@
  */
 int print_last_digit(int n)
 {
-	if (n < 0)
-	{
-		n = n * -1;
-	}
-	_putchar((n % 10) + '0');
+	int a;
 
-	return (n % 10);
+	if (n < 0)
+		n = -n;
+
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+
+	_putchar(a + '0');
+
+	return (a);
 }
