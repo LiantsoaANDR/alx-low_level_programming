f7053e4469edf324d2c34b449747b3d244e0d32c
LiansoaANDR <liantsoa.cpro@gmail.com>
2023-07-18 17:49:41 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - Adds two inegers
+ *
+ * @a: a int to add to b
+ * @b: the int to add to a
+ *
+ * Return: an int
+ *
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
