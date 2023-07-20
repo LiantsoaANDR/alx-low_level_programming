220aff00181f2cf04850480f54b20412d28abc0e
LiansoaANDR <liantsoa.cpro@gmail.com>
2023-07-20 07:03:52 -0700

    Task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints 1 to 100
+ * multiples of three are replaced by Fizz
+ * multiples of five replaced by Buzz
+ * multiples of both replaced by FizzBuzz
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if ((i % 3) == 0 && (i % 5) != 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((i % 5) == 0 && (i % 3) != 0)
+		{
+			printf("Buzz ");
+		}
+		else if ((i % 15) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+
+	return (0);
+}
