a0e17a0c66d0f1cbc5955c1b785e688a31cc6305
LiansoaANDR <liantsoa.cpro@gmail.com>
2023-07-20 07:14:26 -0700

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i < 101; i++)
+	for (i = 1; i < 100; i++)
 	{
 		if ((i % 3) == 0 && (i % 5) != 0)
 		{
@@ -31,7 +31,7 @@ int main(void)
 			printf("%d ", i);
 		}
 	}
-	printf("\n");
+	printf("Buzz\n");
 
 	return (0);
 }
