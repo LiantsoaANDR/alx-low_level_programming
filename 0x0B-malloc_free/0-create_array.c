f7cd09e833287efb378a4d59bf930c56d9297afb
LiansoaANDR <liantsoa.cpro@gmail.com>
2023-08-08 00:13:50 -0700

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,7 +12,12 @@ char *create_array(unsigned int size, char c)
 	char *array;
 	unsigned int i;
 
+	if (!size)
+		return (0);
+
 	array = malloc(sizeof(char) * size);
+	if (!array)
+		return (0);
 
 	for (i = 0; i < size; i++)
 	{
