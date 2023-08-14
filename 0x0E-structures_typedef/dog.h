928cfc66980ced3733e2a572abb514f8608a91d7
LiansoaANDR <liantsoa.cpro@gmail.com>
2023-08-14 06:46:29 -0700

    Fixing

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_
+#define _DOG_
+
+/**
+ * struct dog - struct that contains its name, age, his owner
+ * @name: pointer to the name of the dog
+ * @age: age of the dog as a float
+ * @owner: pointer to theowner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
