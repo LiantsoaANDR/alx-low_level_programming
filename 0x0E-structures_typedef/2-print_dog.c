89cd404ebfc036e96f38efe17e269b47cda4fb1b
LiansoaANDR <liantsoa.cpro@gmail.com>
2023-08-14 07:26:17 -0700

    Task 1

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_dog - prints each member of the struct dog
+ * @d: pointer to the struct dog
+ * Return: void
+ * if an ele;ent of d is NULL, prints (nil) instead of this element
+ * eg, if name is NULL then prints Name: (nil)
+ * if d is NULL print nothing
+ */
