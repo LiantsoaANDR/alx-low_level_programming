8ae28aae46357ce62f2fbc91896fe7a51bef5e17
LiansoaANDR <liantsoa.cpro@gmail.com>
2023-08-17 10:32:31 -0700

    Task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,56 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ * print_all - prints anything
+ * @format: is a list of types of argumemts passed to the fuction
+ * the formats are:
+ * c for char
+ * i for int
+ * f for float
+ * s for char * (if the string is NULL, print (nil) instead)
+ * any other char sould be ignored
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	va_list list;
+	int i = 0, l = 0;
+	int ci;
+	double f;
+	char *string;
+
+	while (format[l])
+		l++;
+	va_start(list, format);
+	while (format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				ci = va_arg(list, int);
+				printf("%c", ci);
+				break;
+			case 'i':
+				ci = va_arg(list, int);
+				printf("%d", ci);
+				break;
+			case 'f':
+				f = va_arg(list, double);
+				printf("%f", f);
+				break;
+			case 's':
+				string = va_arg(list, char *);
+				if (!string)
+					string = "(nil)";
+				printf("%s", string);
+				break;
+			default:
+				break;
+		}
+		if (i < l - 1)
+			printf(", ");
+		i++;
+	}
+	va_end(list);
+	printf("\n");
+}
