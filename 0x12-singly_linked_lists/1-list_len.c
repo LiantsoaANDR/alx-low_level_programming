d285790a3417e3e381fdbdae85100922141a4957
LiansoaANDR <liantsoa.cpro@gmail.com>
2023-08-24 03:43:54 -0700

    Task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "lists.h"
 /**
  * list_len - give the number of elements in a linked list-t list
  * @h: pointer to the list_t list
@@ -6,4 +6,13 @@
  */
 size_t list_len(const list_t *h)
 {
+	size_t len = 0;
+
+	while (h)
+	{
+		len++;
+		h = h->next;
+	}
+
+	return (len);
 }
