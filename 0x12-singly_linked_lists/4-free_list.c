2c2fca0c7fcef7ccbdbd99c366f9cde2f3e4b7ce
LiansoaANDR <liantsoa.cpro@gmail.com>
2023-08-24 04:57:31 -0700

    Task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_list - freees a list_t list
+ * @head: pointer to the header of the list
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *current = head;
+
+	while (current)
+	{
+		current = current->next;
+		free(head);
+		head = current;
+	}
+}
