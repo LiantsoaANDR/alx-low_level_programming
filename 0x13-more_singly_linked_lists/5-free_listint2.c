2af4f5acc3533a19c7c5e685b9502e26a2ec880a
LiansoaANDR <liantsoa.cpro@gmail.com>
2023-08-28 10:50:18 -0700

    Task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,11 +12,11 @@ void free_listint2(listint_t **head)
 
 	if (!head)
 		return;
-	while (current)
+	while (*head)
 	{
 		current = current->next;
 		free(*head);
 		*head = current;
 	}
-	*head = NULL;
+	head = NULL;
 }
