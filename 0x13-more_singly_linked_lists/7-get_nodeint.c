16dac32d23358b9af2f83d3fc2069d59e19924df
LiansoaANDR <liantsoa.cpro@gmail.com>
2023-08-29 05:13:53 -0700

    Task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -8,16 +8,15 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *current = head;
 	unsigned int i = 0;
 
-	while (i < index)
+	while (i < index && head)
 	{
 		i++;
-		current = current->next;
+		head = head->next;
 	}
-	if (!current)
+	if (!head)
 		return (0);
 
-	return (current);
+	return (head);
 }
