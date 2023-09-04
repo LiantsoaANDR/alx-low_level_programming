09d3769d0768d09bd3fa6963fb37214e7f670719
LiansoaANDR <liantsoa.cpro@gmail.com>
2023-09-04 12:09:38 -0700

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!text_content)
 		text_content = "";
-	while (text_content)
+	while (text_content[l])
 		l++;
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
