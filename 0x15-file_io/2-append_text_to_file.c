c6c0fbaef7c1f3e02cbb6179ca84e3f3359b0e92
LiansoaANDR <liantsoa.cpro@gmail.com>
2023-09-04 12:39:24 -0700

    Task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	while (text_content[l])
 		l++;
 
-	fd = open(filename, O_WRONLUY | O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 	d_written = write(fd, text_content, l);
