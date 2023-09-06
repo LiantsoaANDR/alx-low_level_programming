a8f0eaa385c44db53ed934bbb640a0d59daac37f
LiansoaANDR <liantsoa.cpro@gmail.com>
2023-09-06 04:48:51 -0700

    Need to short the code

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -37,7 +37,8 @@
 		if (file_from == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from fil %s\n", argv[1]);
-		exit(98);
+			exit(98);
+		}
 	}
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (file_to == -1)
