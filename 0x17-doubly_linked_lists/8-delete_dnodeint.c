9806700090bfbd6070e7c3f06a1d756c02524b7b
LiantsoaANDR <liantsoa.cpro@gmail.com>
2023-11-09 06:40:03 -0800

    Task 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -23,7 +23,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (1);
 	}
 
-	while (current && i < index - 1)
+	while (current && i < index)
 	{
 		current = current->next;
 		i++;
