60a2ba08423ad86b3b65dc5cee6804299c23272b
LiantsoaANDR <liantsoa.cpro@gmail.com>
2023-11-09 05:27:01 -0800

    task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -21,6 +21,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		new->next = current;
 		new->prev = NULL;
+		if (*h)
+			(*h)->prev = new;
 		*h = new;
 		return (new);
 	}
